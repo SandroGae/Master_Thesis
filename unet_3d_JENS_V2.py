2fc21f63dc6b449b6aa83a1c21473710ec6a6a3e
Sandro_Gaelli <sandro.gaelli@uzh.ch>
2025-10-13 11:14:42 +0200

    sync Notebooks

--- unet_3d_JENS_V2.py
+++ unet_3d_JENS_V2.py
@@ -160,47 +160,36 @@ def conv_block(x, filters, kernel_size=(3,3,3), padding="same"):
     x = layers.LayerNormalization(epsilon=1e-5)(x); x = layers.ELU()(x)
     return x
 
-def unet3d(input_shape=(5,192,240,1), base_filters=16, output_activation="sigmoid"):
+def unet3d(input_shape=(5,192,240,1), base_filters=16, output_activation="tanh"):
     inputs = layers.Input(shape=input_shape)
 
-    # Encoder (4 Downsamplings)
-    c1 = conv_block(inputs,            base_filters)        # bf
-    p1 = layers.MaxPooling3D((1,2,2))(c1)
-
-    c2 = conv_block(p1,                base_filters*2)      # 2bf
-    p2 = layers.MaxPooling3D((1,2,2))(c2)
-
-    c3 = conv_block(p2,                base_filters*4)      # 4bf
-    p3 = layers.MaxPooling3D((1,2,2))(c3)
-
-    c4 = conv_block(p3,                base_filters*8)      # 8bf
-    p4 = layers.MaxPooling3D((1,2,2))(c4)
+    # Encoder (Depth=4)
+    c1 = conv_block(inputs, base_filters)            ; p1 = layers.MaxPooling3D((1,2,2))(c1)
+    c2 = conv_block(p1, base_filters*2)              ; p2 = layers.MaxPooling3D((1,2,2))(c2)
+    c3 = conv_block(p2, base_filters*4)              ; p3 = layers.MaxPooling3D((1,2,2))(c3)
+    c4 = conv_block(p3, base_filters*8)              ; p4 = layers.MaxPooling3D((1,2,2))(c4)
 
     # Bottleneck
-    bn = conv_block(p4,                base_filters*16)     # 16bf
+    bn = conv_block(p4, base_filters*16)
 
     # Decoder
     u4 = layers.Conv3DTranspose(base_filters*8, (1,2,2), (1,2,2), padding="same")(bn)
-    u4 = layers.Concatenate()([u4, c4])
-    c5 = conv_block(u4, base_filters*8)
+    u4 = layers.Concatenate()([u4, c4])              ; c5 = conv_block(u4, base_filters*8)
 
     u3 = layers.Conv3DTranspose(base_filters*4, (1,2,2), (1,2,2), padding="same")(c5)
-    u3 = layers.Concatenate()([u3, c3])
-    c6 = conv_block(u3, base_filters*4)
+    u3 = layers.Concatenate()([u3, c3])              ; c6 = conv_block(u3, base_filters*4)
 
     u2 = layers.Conv3DTranspose(base_filters*2, (1,2,2), (1,2,2), padding="same")(c6)
-    u2 = layers.Concatenate()([u2, c2])
-    c7 = conv_block(u2, base_filters*2)
+    u2 = layers.Concatenate()([u2, c2])              ; c7 = conv_block(u2, base_filters*2)
 
     u1 = layers.Conv3DTranspose(base_filters,   (1,2,2), (1,2,2), padding="same")(c7)
-    u1 = layers.Concatenate()([u1, c1])
-    c8 = conv_block(u1, base_filters)
+    u1 = layers.Concatenate()([u1, c1])              ; c8 = conv_block(u1, base_filters)
 
     out = layers.Conv3D(1, (1,1,1), activation=output_activation,
                         kernel_initializer="glorot_uniform")(c8)
 
-    return models.Model(inputs, out, name=f"3D_U-Net_JENS_d4_bf{{base_filters}}_out{output_activation}")
-
+    return models.Model(inputs, out,
+                        name=f"3D_U-Net_JENS_d4_bf{base_filters}_out{output_activation}")
 
 
 # %%
