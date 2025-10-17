f69e158ef50bb8e447bb931274970cc53a5136fb
Sandro_Gaelli <sandro.gaelli@uzh.ch>
2025-10-17 18:22:09 +0200

    sync Notebooks

--- train_utils.py
+++ train_utils.py
@@ -1,3 +1,14 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.3
+# ---
+
+# %%
 # train_utils.py
 import h5py
 import re as _re
